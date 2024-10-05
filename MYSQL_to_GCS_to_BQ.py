fb8530841a46269c2a77140c831f484539143cff
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-10-05 12:40:49 -0500

    to bq

--- MYSQL_to_GCS_to_BQ.py
+++ MYSQL_to_GCS_to_BQ.py
@@ -3,6 +3,7 @@ from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.providers.google.cloud.transfers.bigquery_to_gcs import BigQueryToGCSOperator
 from airflow.providers.google.cloud.transfers.mysql_to_gcs import MySQLToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.utils.dates import days_ago
 from random import randint
 from datetime import datetime
@@ -20,4 +21,17 @@ with DAG("mysql_to_gcs", start_date=datetime(2024, 9, 1),
         export_format="csv",
     )
 
-    [mysql_to_gcs_export]
+    load_csv_delimiter = GCSToBigQueryOperator(
+        task_id="gcs_to_bigquery_example_delimiter_async",
+        bucket="dag_preview",
+        source_objects=["mysql_to_gcs.csv"],
+        source_format="csv",
+        destination_project_dataset_table=f"dwh-dtp.bdpkmn.mysql_to_bq",
+        write_disposition="WRITE_TRUNCATE",
+        external_table=False,
+        autodetect=True,
+        field_delimiter=",",
+        quote_character="",
+    )
+
+    mysql_to_gcs_export >> load_csv_delimiter
