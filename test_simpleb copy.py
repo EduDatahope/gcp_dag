ac59af5ba7c468acdf63cabfe473a2ff6447febb
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-10-05 10:15:05 -0500

    bq to cs

--- test_simpleb copy.py
+++ test_simpleb copy.py
@@ -13,7 +13,7 @@ with DAG("bigquery_to_gcs", start_date=datetime(2024, 9, 1),
     bigquery_to_gcs = BigQueryToGCSOperator(
         task_id="bigquery_to_gcs",
         source_project_dataset_table=f"dwh-dtp.bdpkmn.tb_gcloud2",
-        destination_cloud_storage_uris=[f"gs://dag_preview/file_dwh-dtp_bdpkmn_tb_gcloud2"],
+        destination_cloud_storage_uris=[f"gs://dag_preview/file_dwh-dtp_bdpkmn_tb_gcloud2.csv"],
     )
 
     [bigquery_to_gcs]
