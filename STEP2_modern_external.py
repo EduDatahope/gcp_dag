be17f43e12fa62791bcf75c6d874462b9cd49c18
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-10-05 18:57:18 -0500

    external

--- STEP2_modern_external.py
+++ STEP2_modern_external.py
@@ -9,8 +9,8 @@ from random import randint
 from datetime import datetime
 
 
-with DAG("gcp_job_sp_ex2", start_date=datetime(2024, 9, 1),
-    schedule_interval="@daily", catchup=False) as dag:
+with DAG("gcp_job_sp_ex2", start_date=datetime(2024, 10, 5),
+    schedule_interval="*/5 * * * *", catchup=False) as dag:
 
      step_gcp_job_sp_ex2 = BigQueryInsertJobOperator(
      task_id="step_gcp_job_sp_ex2",
