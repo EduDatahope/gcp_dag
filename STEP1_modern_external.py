0519ff2e930a32bcb5bf022353c9f89627acfb12
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-10-05 18:01:30 -0500

    Combination last 10 commits - Dags modern correct

--- STEP1_modern_external.py
+++ STEP1_modern_external.py
@@ -1,24 +1,29 @@
-from airflow.decorators import dag , task
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.utils.dates import days_ago
+from random import randint
 from datetime import datetime
 
-@dag(start_date=datetime(2024, 9, 1),schedule="@daily", catchup=False)
-def dag_step1():
 
-     @task
-     def call_stored_procedure1():
-      BigQueryInsertJobOperator(
-      task_id="call_stored_procedure1",
-      configuration={
+with DAG("gcp_job_sp_ex1", start_date=datetime(2024, 10, 5),
+    schedule_interval="*/45 * * * *", catchup=False) as dag:
+
+     step_gcp_job_sp_ex1 = BigQueryInsertJobOperator(
+     task_id="step_gcp_job_sp_ex1",
+     configuration={
         "query": {
-            "query": "CALL `dwh-dtp.bdpkmn.sp_load1`('comp1**ex'); ",
+            "query": "CALL `dwh-dtp.bdpkmn.sp_load1`('comp**ex1'); ",
             "useLegacySql": False,
              }
              },
      location='US',
      )
-     ##comentario si fueran varios pasos
-     ##seria call_stored_procedure1() >> call_stored_procedure2()
-     call_stored_procedure1()
 
-dag_step1()
+     @task
+     def done():
+       print("done :)")
+
+     step_gcp_job_sp_ex1 >> done()
--- STEP2_modern_external.py
+++ STEP2_modern_external.py
@@ -1,29 +1,36 @@
-from airflow.decorators import dag , task
-from airflow.sensors.external_task import ExternalTaskSensor
+from airflow import DAG
+from airflow.decorators import task
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.utils.dates import days_ago
+from random import randint
 from datetime import datetime
 
-@dag(start_date=datetime(2024, 9, 1),schedule="@daily", catchup=False)
-def dag_step2():
 
-     waiting_for_1 = ExternalTaskSensor(
-        task_id = 'waiting_for_1',
-        external_dag_id = 'dag_step1',
-        external_task_id = 'call_stored_procedure1')
+with DAG("gcp_job_sp_ex2", start_date=datetime(2024, 10, 5),
+    schedule_interval="*/45 * * * *", catchup=False) as dag:
 
-     @task
-     def call_stored_procedure2():
-      BigQueryInsertJobOperator(
-      task_id="call_stored_procedure2",
-      configuration={
+     step_gcp_job_sp_ex2 = BigQueryInsertJobOperator(
+     task_id="step_gcp_job_sp_ex2",
+     configuration={
         "query": {
-            "query": "CALL `dwh-dtp.bdpkmn.sp_load2`('comp2**ex'); ",
+            "query": "CALL `dwh-dtp.bdpkmn.sp_load2`('comp**ex2'); ",
             "useLegacySql": False,
              }
              },
      location='US',
      )
 
-     waiting_for_1 >> call_stored_procedure2()
+     waiting_for_1 = ExternalTaskSensor(
+        task_id = 'waiting_for_1',
+        external_dag_id = 'gcp_job_sp_ex1',
+        external_task_id = None,
+        timeout=300)
 
-dag_step2()
+     @task
+     def done():
+        print("done")
+
+     waiting_for_1 >> step_gcp_job_sp_ex2 >> done()
