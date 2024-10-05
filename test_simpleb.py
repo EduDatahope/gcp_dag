439aed4fb2523a06d62f50ca23204e5c8f0d4051
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-10-05 09:32:40 -0500

    v6

--- test_simpleb.py
+++ test_simpleb.py
@@ -11,7 +11,7 @@ with DAG("gcp_job_exec2", start_date=datetime(2024, 9, 1),
     schedule_interval="@daily", catchup=False) as dag:
 
      call_stored_procedure = BigQueryInsertJobOperator(
-     task_id="call_stored_procedure",
+     task_id="call_stored_procedure2",
      configuration={
         "query": {
             "query": "CALL `dwh-dtp.bdpkmn.sp_load2`('comp$$$$$'); ",
@@ -21,4 +21,4 @@ with DAG("gcp_job_exec2", start_date=datetime(2024, 9, 1),
      location='US',
      )
 
-     [call_stored_procedure]
+     [call_stored_procedure2]
