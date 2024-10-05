9e92e8ccff205777795f7afdfc2676ce074b6803
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-10-05 13:42:09 -0500

    dag dependencies

--- MYSQL_to_GCS_to_BQ.py
+++ MYSQL_to_GCS_to_BQ.py
@@ -1,4 +1,5 @@
 from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.providers.google.cloud.transfers.bigquery_to_gcs import BigQueryToGCSOperator
@@ -15,7 +16,7 @@ with DAG("mysql_to_gcs", start_date=datetime(2024, 9, 1),
     mysql_to_gcs_export = MySQLToGCSOperator(
         task_id="mysql_to_gcs_export",
         mysql_conn_id="mysql_conn",
-        sql="SELECT * FROM test1",
+        sql="SELECT * FROM tvw_test",
         bucket="dag_preview",
         filename="mysql_to_gcs.csv",
         export_format="csv",
@@ -34,4 +35,12 @@ with DAG("mysql_to_gcs", start_date=datetime(2024, 9, 1),
         quote_character="",
     )
 
-    mysql_to_gcs_export >> load_csv_delimiter
+    trigger = TriggerDagRunOperator(
+		task_id='trigger_target_dag',
+		trigger_dag_id='postgres_to_gcs',
+		conf={"message": "my_data"},
+		wait_for_completion=False
+	)
+
+
+    mysql_to_gcs_export >> load_csv_delimiter >> trigger
