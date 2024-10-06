cb61c0c8cb0719f8ad305991f087f2043887cd8b
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-10-06 08:50:19 -0500

    fix

---
+++ triger1_modern.py
@@ -0,0 +1,35 @@
+from airflow.decorators import dag , task
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from datetime import datetime
+
+@dag(start_date=datetime(2023, 1, 1), schedule='@daily', catchup=False)
+def triger_1_modern():
+
+	@task
+	def start1():
+	 BigQueryInsertJobOperator(
+     task_id="step_gcp_job_m1",
+     configuration={
+        "query": {
+            "query": "CALL `dwh-dtp.bdpkmn.sp_load1`('trg1**'); ",
+            "useLegacySql": False,
+             }
+             },
+     location='US',
+     )
+
+	trigger = TriggerDagRunOperator(
+		task_id='trigger_target_dag',
+		trigger_dag_id='triger_2_modern',
+		conf={"message": "my_data"},
+		wait_for_completion=True
+	)
+
+	@task
+	def done():
+		print("done")
+
+	start1() >> trigger >> done()
+
+triger_1_modern()
---
+++ triger2_modern.py
@@ -0,0 +1,24 @@
+from airflow.decorators import dag , task
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from datetime import datetime
+
+@dag(start_date=datetime(2023, 1, 1), schedule='@daily', catchup=False)
+def triger_2_modern():
+
+	@task
+	def start2():
+	 BigQueryInsertJobOperator(
+     task_id="step_gcp_job_m2",
+     configuration={
+        "query": {
+            "query": "CALL `dwh-dtp.bdpkmn.sp_load2`('trg2**'); ",
+            "useLegacySql": False,
+             }
+             },
+     location='US',
+     )
+
+    start2()
+
+triger_2_modern()
